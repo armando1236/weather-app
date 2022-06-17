b7915acc9202fcebc935e2b188e22d9d0176b928
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-16 23:41:04 -0700

    completed function for daily

--- Assets/script.js
+++ Assets/script.js
@@ -31,12 +31,24 @@ fiveDays (resultObj);
 function fiveDays (resultObj) {
     
     for (let i = 1; i < 6; i++) {
-        var dayElement = $(`#day${i}`)
-        var tempElement = $('<p></p>')
-        var windElement = $('<p></p>')
-        var humElement = $('<p></p>')
-        var uvElement = $('<p></p>')
+    //     var dayElement = $(`#day${i}`)
+    //     var tempElement = $('<p></p>')
+    //     tempElement.text(resultObj.daily[i].temp.day).append(dayElement)
+    //     console.log(resultObj.daily[i].temp.day)
+    //     var windElement = $('<p></p>')
+    //    windElement.text(resultObj.daily[i].wind_speed).append(dayElement)
+    //     var humElement = $('<p></p>')
+    //     humElement.text(resultObj.daily[i].humidity).append(dayElement)
+    //     var uvElement = $('<p></p>')
+    //     uvElement.text(resultObj.daily[i].uvi).append(dayElement)
         
+
+$(`#day${i}`).append(`<p>Temperature: ${resultObj.daily[i].temp.day}</p>`)
+$(`#day${i}`).append(`<p>Wind: ${resultObj.daily[i].wind_speed}</p>`)
+$(`#day${i}`).append(`<p>Humidity: ${resultObj.daily[i].humidity}</p>`)
+$(`#day${i}`).append(`<p>UV: ${resultObj.daily[i].uvi}</p>`)
+
+
         
     }
 }
