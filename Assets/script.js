16975b6259ef6e6024eec3b83e812ff1ebb80eb4
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-16 19:04:43 -0700

    working on js

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,18 @@
 var url = 'http://api.openweathermap.org/geo/1.0/direct?q=${userInput},3166&limit=1&appid=86b659b4340d86c936cd439ce4e7e614';
-
 fetch(url)
 .then(response =>response.json())
 .then(data =>console.log(data));
-    dataFetch(data.response);
+dataFetch(data.response);
 
-    function dataFetch(data)
+function dataFetch(data) {
+    searchFunction(data);
+    return [latitude, longitude]
+}
+function searchFunction(resultObj) {
     var longitude = dataObject.lon;
     var latitude = dataObject.lat;
+    var searchURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=86b659b4340d86c936cd439ce4e7e614`;
+    fetch(searchURL)
+}
 
-
-
-    https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=86b659b4340d86c936cd439ce4e7e614
+    
