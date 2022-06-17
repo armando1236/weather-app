0478a460d657fd21aa88ae26f14b1c47db468e81
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-17 01:28:15 -0700

    time

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,18 @@
+let currentCity;
 let userSearch; 
 $(document).ready(function () {
     $("#searchBtn").click(function () {
         userSearch = $('#searchBar').val();
+        locationFetch(userSearch); 
     });
 });
 
 var apiKey = '86b659b4340d86c936cd439ce4e7e614';
-var url = `http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=1&appid=${apiKey}`;
+// $('#time').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+setInterval(function (){$('#time').text(moment().format('MMMM Do YYYY, h:mm:ss a'));}, 1000);
 
-
-function locationFetch(){
+function locationFetch(searchRequest){
+    var url = `http://api.openweathermap.org/geo/1.0/direct?q=${searchRequest}&limit=1&appid=${apiKey}`;
     fetch(url)
     .then(response => response.json())
     .then(data => searchFunction(data));
@@ -21,6 +24,7 @@ function searchFunction(resultObj) {
     console.log(resultObj)
     var lon = resultObj[0].lon;
     var lat = resultObj[0].lat;
+    currentCity = resultObj[0].name;
     var searchURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     fetch(searchURL)
         .then(response => response.json())
@@ -29,6 +33,7 @@ function searchFunction(resultObj) {
 
 function weatherPrint(resultObj) {
     console.log(resultObj)
+    $('#currentCity').text(currentCity);
     $('#temp').text(resultObj.current.temp);
     $('#wind').text(resultObj.current.wind_speed);
     $('#hum').text(resultObj.current.humidity);
