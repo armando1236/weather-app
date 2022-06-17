d4652a5ef9d2eed3793fa76b3c3c4792d177e290
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-17 00:28:00 -0700

    updated js

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,19 @@
-var url = `http://api.openweathermap.org/geo/1.0/direct?q=austin&limit=1&appid=86b659b4340d86c936cd439ce4e7e614`;
-// var url = `http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=1&appid=${apiKey}`;
+let userSearch; 
+$(document).ready(function () {
+    $("#searchBtn").click(function () {
+        userSearch = $('#searchBar').val();
+    });
+});
+
 var apiKey = '86b659b4340d86c936cd439ce4e7e614';
-var userSearch = 'austin'
-fetch(url)
-.then(response =>response.json())
-.then(data =>searchFunction(data));
+var url = `http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=1&appid=${apiKey}`;
+
+
+function locationFetch(){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => searchFunction(data));
+}
 
 
 
@@ -14,30 +23,28 @@ function searchFunction(resultObj) {
     var lat = resultObj[0].lat;
     var searchURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     fetch(searchURL)
-    .then(response =>response.json())
-    .then(data =>weatherPrint(data));
+        .then(response => response.json())
+        .then(data => weatherPrint(data));
 }
 
 function weatherPrint(resultObj) {
-console.log(resultObj)
-$('#temp').text(resultObj.current.temp);
-$('#wind').text(resultObj.current.wind_speed);
-$('#hum').text(resultObj.current.humidity);
-$('#uv').text(resultObj.current.uvi);
-fiveDays (resultObj);
+    console.log(resultObj)
+    $('#temp').text(resultObj.current.temp);
+    $('#wind').text(resultObj.current.wind_speed);
+    $('#hum').text(resultObj.current.humidity);
+    $('#uv').text(resultObj.current.uvi);
+    fiveDays(resultObj);
 
 
 }
-function fiveDays (resultObj) {
-    
+function fiveDays(resultObj) {
+
     for (let i = 1; i < 6; i++) {
-$(`#day${i}`).append(`<p>Temperature: ${resultObj.daily[i].temp.day}</p>`)
-$(`#day${i}`).append(`<p>Wind: ${resultObj.daily[i].wind_speed}</p>`)
-$(`#day${i}`).append(`<p>Humidity: ${resultObj.daily[i].humidity}</p>`)
-$(`#day${i}`).append(`<p>UV: ${resultObj.daily[i].uvi}</p>`)
-        
+        $(`#day${i}`).append(`<p>Temperature: ${resultObj.daily[i].temp.day}</p>`)
+        $(`#day${i}`).append(`<p>Wind: ${resultObj.daily[i].wind_speed}</p>`)
+        $(`#day${i}`).append(`<p>Humidity: ${resultObj.daily[i].humidity}</p>`)
+        $(`#day${i}`).append(`<p>UV: ${resultObj.daily[i].uvi}</p>`)
+
     }
 }
-// addEventListener(("click"),$('#searchBtn')) {
-    
-// }
+
