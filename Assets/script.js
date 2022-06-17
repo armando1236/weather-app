f0b324cf5ec354b848202937a4d56dc7269c2393
amjr86-DBxjRd <amjr86@outlook.com>
2022-06-16 20:13:38 -0700

    added to js

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,6 @@
-var url = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput},3166&limit=1&appid=86b659b4340d86c936cd439ce4e7e614`;
+// var url = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput},3166&limit=1&appid=86b659b4340d86c936cd439ce4e7e614`;
+var url = `http://api.openweathermap.org/geo/1.0/direct?q=los20%angeles,3166&limit=1&appid=86b659b4340d86c936cd439ce4e7e614`;
+var apiKey = '86b659b4340d86c936cd439ce4e7e614';
 fetch(url)
 .then(response =>response.json())
 .then(data =>console.log(data));
@@ -8,13 +10,13 @@ searchFunction(data.response);
 function searchFunction(resultObj) {
     var lon = resultObj.lon;
     var lat = resultObj.lat;
-    var searchURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=86b659b4340d86c936cd439ce4e7e614`;
+    var searchURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     fetch(searchURL)
     .then(response =>response.json())
     .then(data =>console.log(data));
-    addEventListener(("click"),$('#searchBtn')) {
-        
-    }
 }
 
+
+addEventListener(("click"),$('#searchBtn')) {
     
+}
